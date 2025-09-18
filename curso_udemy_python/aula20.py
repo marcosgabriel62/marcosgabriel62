82f530b63e00d719029c2e529fba49cde0cfc20f
Marcos Koslovski <marcosgabriel62@gmail.com>
2025-09-18 20:49:25 -0300

    aula de hoje 18/09
    exercicio da aula de hoje

---
+++ curso_udemy_python/aula20.py
@@ -0,0 +1,6 @@
+n1=input('Digite o primeiro valor: ')
+n2=input('Digite o segundo valor: ')
+if n1>n2:
+    print(f'o primeiro valor. {n1}, é maior que o segundo valor, {n2}')
+else:
+    print(f'o segundo valor, {n2}, é maior que o primeiro valor, {n1}')
