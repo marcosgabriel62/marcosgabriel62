66620a30b03a00694c28dba7280ea88688f980ba
Marcos Koslovski <marcosgabriel62@gmail.com>
2025-10-04 10:03:22 -0300

    termino exercicio aula 53
    execução exercicio aula 53

--- curso_udemy_python/aula53-exercicio.py
+++ curso_udemy_python/aula53-exercicio.py
@@ -7,36 +7,58 @@ erros de índices inexistentes na lista.
 """
 
 import os
+import time
 
 lista=[]
 
+def clear_terminal():
+    _ = os.system('cls')
+
 try:
+    clear_terminal()
     while True:
-        opcao=input(20*'*'+'\n' + 'Digite a opção:\nA - adicionar\nB - Apagar\nC - Listar itens/indices\n' + 20*'*' + '\n').upper()
+        opcao=input('A - Adicionar\nB - Apagar\nC - Listar itens/indices\nD - Sair\nDigite a opção: ').upper()
 
         if opcao=='A':
+            clear_terminal()
             material=input('Digite o que você quer incluir na lista: ')
             lista.append(material)
             print(f'{material} incluido na lista!')
-
+            continuar=input('Pressione qualquer tecla para continuar....')
+            clear_terminal()
+            
         elif opcao=='B':
-            indice=input('Digite a posição da lista que você quer apagar')
-            indice_int=int(indice)
-            indices = range(len(lista))
-            if indice_int in indices:
-                del lista[indice_int]
-                print('Item apagado!')
+            clear_terminal()
+            indice=input('Digite a posição da lista que você quer apagar: ')
+            if indice.isdigit():
+                indice_int=int(indice)
+                indices = range(len(lista))
+                if indice_int in indices:
+                    del lista[indice_int]
+                    print(f'Item {indice_int} da lista apagado!')
+                else:
+                    print(f'Digite um numero de indice válido! Não há nenhum item no indice {indice_int}')
             else:
-                print('Digite um indice válido')
+                print('Não foi digitado um numero, tente novamente.')
+            continuar=input('Pressione qualquer tecla para continuar....')
+            clear_terminal()
 
         elif opcao=='C':
+            clear_terminal()
             indices = range(len(lista))
             for indice in indices:
                 print(indice, lista[indice])
-            
+            continuar=input('Pressione qualquer tecla para continuar....')
+            clear_terminal()
+
+        elif opcao=='D':
+            break
+   
         else:
             print('Opção inválida! tente novamente!')
-
+            continuar=input('Pressione qualquer tecla para continuar....')
+            clear_terminal()
 
 except:
     print('Erro desconhecido')
+    
